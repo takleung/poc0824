88122dcfc5790e23046657e20b25d2b9b0fde02c
takleung <takleung@gmail.com>
2020-09-08 13:06:24 +0800

    Network policy 1st release

--- NetworkPolicy/00-startup.sh
+++ NetworkPolicy/00-startup.sh
@@ -1,2 +1,7 @@
 oc new-project poc-network-policy
-oc new-app --name gitlab --docker-image gitlab/gitlab-ce:8.4.3-ce.0
+oc new-app \
+    -e MYSQL_USER=admin \
+    -e MYSQL_PASSWORD=redhat \
+    -e MYSQL_DATABASE=mysqldb \
+    registry.redhat.io/rhscl/mysql-80-rhel7
+mysql -u admin -predhat -h 10.131.2.143 mysqldb
