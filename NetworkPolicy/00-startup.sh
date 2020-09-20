dedfb29010df08e34d41a9916838a3f164ac6fb1
takleung <takleung@gmail.com>
2020-09-20 13:46:52 +0800

    start with PoC scripts writing

--- NetworkPolicy/00-startup.sh
+++ NetworkPolicy/00-startup.sh
@@ -20,5 +20,23 @@ oc new-app httpd
 mysql -u admin -predhat -h 10.129.2.27 mysqldb
 mysql -u admin -predhat -h 172.30.108.31 mysqldb
 
+********* batch script *********
+oc get namespace coolstore -o jsonpath='{.metadata.name}{" "}{.metadata.creationTimestamp}{"\n"}'
+oc exec \
+    $(oc get pods -n coolstore -l deploymentconfig=catalog -o jsonpath='{range .items[*]}{.metadata.name}{end}') \
+    -n coolstore \
+    -- curl \
+    $(oc get pods -n coolstore -l deploymentconfig=inventory -o jsonpath='{range .items[*]}{.status.podIP}{":8080"}{end}')
+
+cat <<EOF | oc create -f -
+kind: NetworkPolicy
+apiVersion: networking.k8s.io/v1
+metadata:
+  name: deny-by-default
+  namespace: coolstore
+spec:
+  podSelector:
+  ingress: []
+EOF
 
 
---
+++ skopeo/scrtips.sh
@@ -0,0 +1,8 @@
+oc exec skopeo-bp3rq -- \
+    skopeo copy \
+    docker://default-route-openshift-image-registry.apps.poc1.rhocp.hkjc/coolstore/pricing:latest \
+    docker://quay.apps.poc1.rhocp.hkjc/quay/pricing:latest \
+    --src-creds admin:Rh7LZK3Z-9xGaGtgGDVCgPkyqGOipSdCq8eOB_uJF3A \
+    --dest-creds quay:password \
+    --src-tls-verify=false \
+    --dest-tls-verify=false
